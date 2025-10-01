fcc2fc4be297b848bf74b8385c9dddf49634006c
Tony <hoanganhtran1998@gmail.com>
2025-10-01 16:22:46 -0400

    fix build lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { Box, Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
 
 import TranslatePage from "./pages/TranslatePage";
 import BatchTranslatePage from "./pages/BatchTranslatePage";
